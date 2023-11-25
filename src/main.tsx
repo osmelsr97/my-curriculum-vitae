d28f9cf42acde3648bd9ca14fbaa71f361441175
Osmel Smith <osmelsr97@gmail.com>
2023-11-25 17:41:47 -0300

    Setup Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { RouterProvider } from "@/components";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1 className="text-hoki-300">My curriculum vitae</h1>
+    <RouterProvider />
   </React.StrictMode>
 );
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export {default as Home} from './home/Home'    
+export {default as Error404} from './Error404Page/Error404Page'    
