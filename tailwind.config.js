0d314aca55874d25edad357fb70c163c8c1b8e21
Osmel Smith <osmelsr97@gmail.com>
2023-11-25 15:16:37 -0300

    Fonts Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "sans"],
+      },
+    },
   },
   plugins: [],
 };
